8cdf61ca7973e270ef2335af3bf31efb496cc0f1
micmarsh <michael.marsh42@gmail.com>
2014-03-08 18:21:06 -0500

    starting chapter 3 exercises

--- ch03/bookstore.hs
+++ ch03/bookstore.hs
@@ -1,9 +1,14 @@
 module Main where
 
-data Book = Book Int String [String]
+data Book = Book Integer String [String]
                 deriving (Show)
 
-data Magazine = Magazine Int String [String]
+data Magazine = Magazine Integer String [String]
                 deriving (Show)
 
-main = getLine >>= putStrLn
+data BookReview = BookReview Book CustomerID String
+
+myInfo = Book 9780135072455 "Algebra of Programming"
+         ["Richard Bird", "Oege de Moor"]
+
+main = getLine >>= putStrLn 
---
+++ ch03/exerises.hs
@@ -0,0 +1,6 @@
+
+count list = count' 0 list 
+    where count' n [] = n
+          count' n (h:tail) = count' (n + 1) tail
+
+main = putStrLn $ show (count [1..5] == length [1..5])
